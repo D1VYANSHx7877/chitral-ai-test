e3953c218bfb34ad1caabb496dd59d4ebccba3e3
Divyansh Dhadhich <divyanshdhadhich@gmail.com>
2025-12-14 18:15:58 +0530

    fix: add binary file support to serverless handler

--- api/index.js
+++ api/index.js
@@ -6,8 +6,9 @@ import app from '../backend/server.js';
 process.env.VERCEL = '1';
 
 // Wrap Express app as a serverless handler
-const handler = serverless(app);
+const handler = serverless(app, {
+  binary: ['application/octet-stream', 'font/woff2'],
+});
 
 // Export for Vercel
 export default handler;
-
